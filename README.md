63170ebdb9aa2250e27db58969bb390aa791f743
Shirisha-abbaraboina <shirisha.abbaraboina@gmail.com>
2024-05-15 10:29:02 +0530

    Day2 Ignity APP

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-#Namaste ReactJS
+#  Namaste React 
+
+# Paracel 
+- Dev Bulid
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Alogorithm - written in C++
+- Paracel is also Caching - Faster Builds
+- Image optimization
+- if i create production bulid -minification
+- Bundling
+- compress files
+- consistent Hashing
+- code Splitting
+- Differential Bundling - Support older Browsers
+- Diagonstic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Different Dev and prod  bundles 
--- app.js
+++ app.js
@@ -1,22 +1,9 @@
-/**
- * <div id="parent">
- *        <div id="child">
- *            <h1>I'm H1 Tag</h1>
- *            <h2>I'm H2 Tag</h2>
- *        </div>
- *        <div id="child2">
- *            <h1>I'm H1 Tag</h1>
- *            <h2>I'm H2 Tag</h2>
- *        </div>
- * </div>
- * 
- * 
- * 
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div",{id:"parent"},
 [React.createElement("div",{id:"child"},[
-    React.createElement("h1",{},"Im H1 Tag"),
+    React.createElement("h1",{},"Namaste React JS"),
     React.createElement("h2",{},"Im H2 Tag")]
     ),
     React.createElement("div",{id:"child2"},[
@@ -24,21 +11,16 @@ const parent = React.createElement("div",{id:"parent"},
         React.createElement("h2",{},"Im H2 Tag")]
         )]
 );
-
 //JSX
-
-
 // const heading = React.createElement(
 //     "h1",
 //     {id:"heading", xyz:"abc"},
 //     "Hello World from React!"
 // );
-
 console.log(parent)
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(parent);
+//Igniting APP
 
 
 

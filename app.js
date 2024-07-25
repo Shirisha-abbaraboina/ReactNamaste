3859bc123eba53c939a83de3bbb50f2ee5e64b51
Shirisha-abbaraboina <shirisha.abbaraboina@gmail.com>
2024-07-25 10:04:02 +0530

    Latest code upto date

--- app.js
+++ app.js
@@ -1,27 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",{id:"parent"},
-[React.createElement("div",{id:"child"},[
-    React.createElement("h1",{},"Namaste React JS"),
-    React.createElement("h2",{},"Im H2 Tag")]
-    ),
-    React.createElement("div",{id:"child2"},[
-        React.createElement("h1",{},"Im H1 Tag"),
-        React.createElement("h2",{},"Im H2 Tag")]
-        )]
-);
-//JSX
-// const heading = React.createElement(
-//     "h1",
-//     {id:"heading", xyz:"abc"},
-//     "Hello World from React!"
-// );
-console.log(parent)
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-//Igniting APP
+//React Element
+const heading = <h1 className="head">Namaste React Using JSX</h1>
 
+//React Functional Component
+const Headingcomponent = () => {
+
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
 
 
 
